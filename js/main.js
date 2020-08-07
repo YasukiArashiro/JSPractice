f9535d1040ec5bd9e3983a59071cd272f4f44d2e
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-08-07 21:54:09 +0900

    [Add]String

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@
 console.log('Hello');
 console.log('Hello');
 
-console.log("it's me!")
+console.log("it's me!");
+console.log("it's me!");
+console.log('it\'s me!');
+
+console.log('hel\nlo wor\tld');
+console.log('hello' + 'world');
+
