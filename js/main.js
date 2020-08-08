4f2ccd51e610dfd128c5eb400a5d9817fcd15fc0
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-08-08 23:38:39 +0900

    [Add]Arithmetic operation

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ console.log('it\'s me!');
 console.log('hel\nlo wor\tld');
 console.log('hello' + 'world');
 
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3); //10の３乗
+
+console.log(2 + 10 * 3); //32
+console.log((2 + 10) * 3); //36
+
+
