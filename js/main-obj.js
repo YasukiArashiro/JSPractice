7c2d18eb176103af4cb1da8a87b59da8c49f11ba
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-08-30 22:27:12 +0900

    [Add]Arrays and loops

--- js/main-obj.js
+++ js/main-obj.js
@@ -11,7 +11,6 @@
 	console.log('↓配列のインデックスが１番目の値を表示')
 	console.log(scores[1]);
 
-
 	console.log('配列の値を変更する')
 	scores[2] = 44;
 	console.log('scores[2] = 44; で変更後の配列↓')
@@ -21,5 +20,16 @@
 
 	console.log('↓console.log(scores.length); を使って配列の要素の数を表示')
 	console.log(scores.length);
+
+
+//配列とループ
+
+	console.log(`Score: ${scores[0]}`);
+	console.log(`Score: ${scores[1]}`);
+	console.log(`Score: ${scores[2]}`);
+
+	for (let i = 0; i < scores.length; i++) {
+		console.log(`Score ${i}: ${scores[i]}`);
+	}
 
 }
