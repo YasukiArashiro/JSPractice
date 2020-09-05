64a8dca091357e9898899bc41cdcd5ed791ee061
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-09-05 18:20:57 +0900

    [Add]Map

--- js/main-obj.js
+++ js/main-obj.js
@@ -103,5 +103,14 @@
 		console.log(`Score ${index}: ${array}`);
 	});
 
+//map()
+
+	const prices = [180, 190, 200];
+
+	const updatedPrices = prices.map((price) => {
+		return price + 20;
+	});
+
+	console.log(updatedPrices);
 
 }
