3d11f07bc4adeda70ee081b580da64c5b84c6e38
YasukiArashiro <yasuki.arashiro1108@gmail.com>
2020-10-04 20:26:54 +0900

    [Add]Multiple elements

--- js/main-dom.js
+++ js/main-dom.js
@@ -7,9 +7,18 @@
 			//querySlector()というメソッドを使うことができる。
 			//中身のテキストはtextContentで扱うことができる。
 
-		//document.querySlector('#target').textContent = 'Changed!';
+		//document.querySelector('#target').textContent = 'Changed!';
 		document.getElementById('target').textContent = 'Changed!!!';
 		//このように２つの書き方ができる。getElementByIdの時は、taegetに#を付けなくてもよい
+
+		//document.querySelector('p').textContent = 'pを書き換えました！！'; ※最初にpだけ書き換えられる
+
+		//document.querySelectorAll('p')[1].textContent = 'pを書き換えました！！';
+		//↑上から1番目のpを書き換える！
+
+		document.querySelectorAll('p').forEach((p, index) => {
+			p.textContent = `${index}番目のpです！`
+		})
 	}
 
 	setTimeout(update, 1000);　//１秒後にupdateを実行する
